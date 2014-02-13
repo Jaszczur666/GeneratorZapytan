51456cda614640fe88971fb5ddd45723c59388bf
Jarosław Komar <jaszczur666@o2.pl>
2014-02-13 07:57:44 +0100

    Initial commit

---
+++ GeneratorZapytan/GeneratorZapytan.cpp
@@ -0,0 +1,46 @@
+// GeneratorZapytan.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+cout<<"	[timeout:500]"<<endl;
+cout<<";"<<endl;
+cout<<"("<<endl;
+//begin loop
+cout<<"  node"<<endl;
+cout<<"    [\"amenity\"=\"place_of_worship\"]"<<endl;
+cout<<"    [\"denomination\"!~\".\"]"<<endl;
+cout<<"   (area:3600224457);"<<endl;
+cout<<"  node"<<endl;
+cout<<"    [\"amenity\"=\"place_of_worship\"]"<<endl;
+cout<<"    [\"name\"!~\".\"]"<<endl;
+cout<<"   (area:3600224457);"<<endl;
+cout<<"  way"<<endl;
+cout<<"    [\"amenity\"=\"place_of_worship\"]"<<endl;
+cout<<"    [\"denomination\"!~\".\"]"<<endl;
+cout<<"    (area:3600224457);"<<endl;
+cout<<"  way"<<endl;
+cout<<"    [\"amenity\"=\"place_of_worship\"]"<<endl;
+cout<<"    [\"name\"!~\".\"]"<<endl;
+cout<<"    (area:3600224457);"<<endl;
+cout<<"  relation"<<endl;
+cout<<"    [\"amenity\"=\"place_of_worship\"]"<<endl;
+cout<<"    [\"denomination\"!~\".\"]"<<endl;
+cout<<"    (area:3600224457);"<<endl;
+cout<<"  relation"<<endl;
+cout<<"    [\"amenity\"=\"place_of_worship\"]"<<endl;
+cout<<"    [\"name\"!~\".\"]"<<endl;
+cout<<"    (area:3600224457);"<<endl;
+cout<<");"<<endl;
+cout<<"("<<endl;
+cout<<"  ._;"<<endl;
+cout<<"  >;"<<endl;
+cout<<");"<<endl;
+cout<<"out meta"<<endl;
+	return 0;
+}
+
